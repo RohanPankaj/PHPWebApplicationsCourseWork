3210d8f4a885bee823c624ae2f63550e6b9085b4
Rohan Pankaj <rohansp01@gmail.com>
2019-11-23 18:59:09 -0800

    first web app

---
+++ index.php
@@ -0,0 +1,27 @@
+<head>
+    <title>Rohan Pankaj</title>
+</head>
+
+<body>
+    <h1>Rohan Pankaj</h1>
+<pre>
+    ---------
+    |        |
+    |        |
+    |        |
+    ---------
+    |\
+    | \
+    |  \
+    |   \
+    |    \
+    |     \
+    </pre>
+
+</body>
+<p>
+<?php 
+  print hash('sha256','Rohan Pankaj');
+?>
+</p>
+
